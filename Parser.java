f29965e67899dc2030feca9581c459c714e91cc6
reflectronic <john-tur@outlook.com>
2020-10-06 15:40:33 -0400

    Add empty parser file

---
+++ Parser.java
@@ -0,0 +1,16 @@
+typ// Operators should be processed in this priority:
+// MULTIPLY/DIVIDE
+// ADD/SUBTRACT
+// EQUALS
+
+//      +
+//     / \
+//   3x   *
+//       / \
+//      4   5
+
+// 3x + 4 * 5 = 6 + 7 * 8
+
+public class Parser {
+    
+}
