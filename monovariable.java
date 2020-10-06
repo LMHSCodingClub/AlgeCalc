07ef2aa01c1b9f027fa1cdbd4ed2569937ac047d
David-Volchonok <57112433+David-Volchonok@users.noreply.github.com>
2020-10-06 15:40:35 -0400

    Create monovariable.java

---
+++ monovariable.java
@@ -0,0 +1,50 @@
+import java.lang.Math; 
+
+class Main
+{
+     public static void main(String[] args)
+     {
+      
+      
+     }
+    
+    public static double monovar()
+    {
+      double a1 = 1.1; // random inputs
+      double a2 = 2.1;
+      double a3 = 1.1;
+      double a4 = 2.1;
+      double a5 = 1.1;
+      
+      double[]arr = alphebatize(a1, a2, a3, a4, a5);
+       
+      // double b = [0]arr; figure out array usage
+      depress(b, c, d, e); // return somehow
+        
+      return 0.0;
+    }
+    
+    public static double alphabetize(a1, a2, a3, a4, a5)
+    {
+      double b = a2/a1;
+      double c = a3/a1;
+      double d = a4/a1;
+      double e = a5/a1;
+       
+      double[]arr = {b, c, ,d , e};
+        
+      return arr;
+    }
+    
+    public static double depress(b, c, d, e)
+    {
+      
+        
+        
+        
+      return ; // figure out
+    }
+    
+    
+    
+}
