830bfd9670091437b727ee5fdbd88cd803f59b3f
reflectronic <john-tur@outlook.com>
2021-02-22 19:02:01 -0500

    Begin ComplexNumber class & abstractions

---
+++ OperationalEntity.java
@@ -0,0 +1,9 @@
+interface OperationalEntity {
+    OperationalEntity plus(OperationalEntity addend);
+
+    OperationalEntity minus(OperationalEntity subtrahend);
+
+    OperationalEntity times(OperationalEntity factor);
+
+    OperationalEntity dividedBy(OperationalEntity dividend);
+}
